9e06a4bb7e5c58382c24740b73b3227062cb5702
Julius C. Duque <jcduque@lycos.com>
2003-06-02 08:10:16 +0000

    Import of JCDUQUE/Crypt-Misty1-1.1.0 from CPAN.
    gitpan-cpan-distribution: Crypt-Misty1
    gitpan-cpan-version:      1.1.0
    gitpan-cpan-path:         JCDUQUE/Crypt-Misty1-1.1.0.tar.gz
    gitpan-cpan-author:       JCDUQUE
    gitpan-cpan-maturity:     released

--- Misty1.xs
+++ Misty1.xs
@@ -5,11 +5,10 @@
 #include "_misty1.c"
 
 typedef struct misty1 {
-    unsigned char *key;
-    unsigned char *buf;
+    unsigned int key[32];
 }* Crypt__Misty1;
 
-MODULE = Crypt::Misty1		PACKAGE = Crypt::Misty1		
+MODULE = Crypt::Misty1		PACKAGE = Crypt::Misty1
 PROTOTYPES: DISABLE
 
 int
@@ -34,14 +33,54 @@ new(class, rawkey)
     {
         STRLEN keyLength;
         if (! SvPOK(rawkey))
-            croak("Error initialization: key must be a NUL-terminated string!");
+            croak("Key setup error: Key must be a string scalar!");
 
         keyLength = SvCUR(rawkey);
         if (keyLength != 16)
-            croak("Error initialization: key must be 16 bytes in length!");
+            croak("Key setup error: Key must be 16 bytes long!");
 
         Newz(0, RETVAL, 1, struct misty1);
-        RETVAL->key = SvPV_nolen(rawkey);
+        keyinit(SvPV_nolen(rawkey), RETVAL->key);
+    }
+
+    OUTPUT:
+        RETVAL
+
+SV*
+encrypt(self, input)
+    Crypt::Misty1 self
+    SV* input
+    CODE:
+    {
+        STRLEN blockSize;
+        void* intext = SvPV(input, blockSize);
+        if (blockSize != 8) {
+            croak("Encryption error: Block size must be 8 bytes long!");
+            RETVAL = newSVpv("", 0);
+        } else {
+            RETVAL = newSVpv("", blockSize);
+            misty1_encrypt(self->key, intext, SvPV_nolen(RETVAL));
+        }
+    }
+
+    OUTPUT:
+        RETVAL
+
+SV*
+decrypt(self, input)
+    Crypt::Misty1 self
+    SV* input
+    CODE:
+    {
+        STRLEN blockSize;
+        void* intext = SvPV(input, blockSize);
+        if (blockSize != 8) {
+            croak("Decryption error: Block size must be 8 bytes long!");
+            RETVAL = newSVpv("", 0);
+        } else {
+            RETVAL = newSVpv("", blockSize);
+            misty1_decrypt(self->key, intext, SvPV_nolen(RETVAL));
+        }
     }
 
     OUTPUT:
@@ -52,27 +91,4 @@ DESTROY(self)
     Crypt::Misty1 self
     CODE:
         Safefree(self);
-
-SV*
-encrypt(self, input)
-    Crypt::Misty1 self
-    SV* input
-    CODE:
-    {
-        STRLEN blockSize;
-        unsigned char *rawbytes;
-        rawbytes = SvPV(input, blockSize);
-        if (blockSize != 8) {
-            croak("Error encryption: block size must be 8 bytes in length!");
-            RETVAL = newSVpv("", 0);
-        } else {
-            RETVAL = NEWSV(0, 8);
-            SvPOK_only(RETVAL);
-            SvCUR_set(RETVAL, 8);
-            misty1(rawbytes, self->key, 1, 0);
-        }
-    }
-
-    OUTPUT:
-        RETVAL
 
--- _misty1.c
+++ _misty1.c
@@ -1,263 +1,334 @@
-/****************************************************
-*
-* MISTY1 Block Cipher Algorithm (8-round/ECB) *
-*
-* Language : Highly Portable C Language *
-* Coding by : Mitsuru Matsui / 22 July 1996 *
-* Copyright : Mitsubishi Electric Coporation *
-*
-* Slight modifications by Julius C. Duque <jcduque (AT) lycos (DOT) com>
-* 2003 May 28th
-*
-****************************************************/
+/* _misty1.c - An implementation of the MISTY1 block cipher, as
+ * described in RFC 2994.
+ *
+ * Copyright (C) 2003 Julius C. Duque <jcduque (AT) lycos (DOT) com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ */
 
 #include <stdio.h>
 
-unsigned short EXTKEY[4][8];
+unsigned int EK[32];
 
-static unsigned char S7[128] = {
-27,  50,  51,  90,  59,  16,  23,  84,  91,  26, 114, 115, 107,  44, 102,  73,
-31,  36,  19, 108,  55,  46,  63,  74,  93,  15,  64,  86,  37,  81,  28,   4,
-11,  70,  32,  13, 123,  53,  68,  66,  43,  30,  65,  20,  75, 121,  21, 111,
-14,  85,   9,  54, 116,  12, 103,  83,  40,  10, 126,  56,   2,   7,  96,  41,
-25,  18, 101,  47,  48,  57,   8, 104,  95, 120,  42,  76, 100,  69, 117,  61,
-89,  72,   3,  87, 124,  79,  98,  60,  29,  33,  94,  39, 106, 112,  77,  58,
- 1, 109, 110,  99,  24, 119,  35,   5,  38, 118,   0,  49,  45, 122, 127,  97,
-80,  34,  17,  6,   71,  22,  82,  78, 113,  62, 105,  67,  52,  92,  88, 125
+static unsigned char s7[128] = {
+    27,  50,  51,  90,  59,  16,  23,  84,  91,  26, 114, 115, 107,
+    44, 102,  73,  31,  36,  19, 108,  55,  46,  63,  74,  93,  15,
+    64,  86,  37,  81,  28,   4,  11,  70,  32,  13, 123,  53,  68,
+    66,  43,  30,  65,  20,  75, 121,  21, 111,  14,  85,   9,  54,
+   116,  12, 103,  83,  40,  10, 126,  56,   2,   7,  96,  41,  25,
+    18, 101,  47,  48,  57,   8, 104,  95, 120,  42,  76, 100,  69,
+   117,  61,  89,  72,   3,  87, 124,  79,  98,  60,  29,  33,  94,
+    39, 106, 112,  77,  58,   1, 109, 110,  99,  24, 119,  35,   5,
+    38, 118,   0,  49,  45, 122, 127,  97,  80,  34,  17,   6,  71,
+    22,  82,  78, 113,  62, 105,  67,  52,  92,  88, 125
 };
 
-static unsigned short S9[512] = {
-451, 203, 339, 415, 483, 233, 251,  53, 385, 185, 279, 491, 307,   9,  45, 211,
-199, 330,  55, 126, 235, 356, 403, 472, 163, 286,  85,  44,  29, 418, 355, 280,
-331, 338, 466,  15,  43,  48, 314, 229, 273, 312, 398,  99, 227, 200, 500,  27,
-  1, 157, 248, 416, 365, 499,  28, 326, 125, 209, 130, 490, 387, 301, 244, 414,
-467, 221, 482, 296, 480, 236,  89, 145,  17, 303,  38, 220, 176, 396, 271, 503,
-231, 364, 182, 249, 216, 337, 257, 332, 259, 184, 340, 299, 430,  23, 113,  12,
- 71,  88, 127, 420, 308, 297, 132, 349, 413, 434, 419,  72, 124,  81, 458,  35,
-317, 423, 357,  59,  66, 218, 402, 206, 193, 107, 159, 497, 300, 388, 250, 406,
-481, 361, 381,  49, 384, 266, 148, 474, 390, 318, 284,  96, 373, 463, 103, 281,
-101, 104, 153, 336,   8,   7, 380, 183,  36,  25, 222, 295, 219, 228, 425,  82,
-265, 144, 412, 449,  40, 435, 309, 362, 374, 223, 485, 392, 197, 366, 478, 433,
-195, 479,  54, 238, 494, 240, 147,  73, 154, 438, 105, 129, 293,  11,  94, 180,
-329, 455, 372,  62, 315, 439, 142, 454, 174,  16, 149, 495,  78, 242, 509, 133,
-253, 246, 160, 367, 131, 138, 342, 155, 316, 263, 359, 152, 464, 489,   3, 510,
-189, 290, 137, 210, 399,  18,  51, 106, 322, 237, 368, 283, 226, 335, 344, 305,
-327,  93, 275, 461, 121, 353, 421, 377, 158, 436, 204,  34, 306,  26, 232,   4,
-391, 493, 407,  57, 447, 471,  39, 395, 198, 156, 208, 334, 108,  52, 498, 110,
-202,  37, 186, 401, 254,  19, 262,  47, 429, 370, 475, 192, 267, 470, 245, 492,
-269, 118, 276, 427, 117, 268, 484, 345,  84, 287,  75, 196, 446, 247,  41, 164,
- 14, 496, 119,  77, 378, 134, 139, 179, 369, 191, 270, 260, 151, 347, 352, 360,
-215, 187, 102, 462, 252, 146, 453, 111,  22,  74, 161, 313, 175, 241, 400,  10,
-426, 323, 379,  86, 397, 358, 212, 507, 333, 404, 410, 135, 504, 291, 167, 440,
-321,  60, 505, 320,  42, 341, 282, 417, 408, 213, 294, 431,  97, 302, 343, 476,
-114, 394, 170, 150, 277, 239,  69, 123, 141, 325,  83,  95, 376, 178,  46,  32,
-469,  63, 457, 487, 428,  68,  56,  20, 177, 363, 171, 181,  90, 386, 456, 468,
- 24, 375, 100, 207, 109, 256, 409, 304, 346,   5, 288, 443, 445, 224,  79, 214,
-319, 452, 298,  21,   6, 255, 411, 166,  67, 136,  80, 351, 488, 289, 115, 382,
-188, 194, 201, 371, 393, 501, 116, 460, 486, 424, 405,  31,  65,  13, 442,  50,
- 61, 465, 128, 168,  87, 441, 354, 328, 217, 261,  98, 122,  33, 511, 274, 264,
-448, 169, 285, 432, 422, 205, 243,  92, 258,  91, 473, 324, 502, 173, 165,  58,
-459, 310, 383,  70, 225,  30, 477, 230, 311, 506, 389, 140, 143,  64, 437, 190,
-120,   0, 172, 272, 350, 292,   2, 444, 162, 234, 112, 508, 278, 348,  76, 450
+static unsigned short s9[512] = {
+    451, 203, 339, 415, 483, 233, 251,  53, 385, 185, 279, 491, 307,
+      9,  45, 211, 199, 330,  55, 126, 235, 356, 403, 472, 163, 286,
+     85,  44,  29, 418, 355, 280, 331, 338, 466,  15,  43,  48, 314,
+    229, 273, 312, 398,  99, 227, 200, 500,  27,   1, 157, 248, 416,
+    365, 499,  28, 326, 125, 209, 130, 490, 387, 301, 244, 414, 467,
+    221, 482, 296, 480, 236,  89, 145,  17, 303,  38, 220, 176, 396,
+    271, 503, 231, 364, 182, 249, 216, 337, 257, 332, 259, 184, 340,
+    299, 430,  23, 113,  12,  71,  88, 127, 420, 308, 297, 132, 349,
+    413, 434, 419,  72, 124,  81, 458,  35, 317, 423, 357,  59,  66,
+    218, 402, 206, 193, 107, 159, 497, 300, 388, 250, 406, 481, 361,
+    381,  49, 384, 266, 148, 474, 390, 318, 284,  96, 373, 463, 103,
+    281, 101, 104, 153, 336,   8,   7, 380, 183,  36,  25, 222, 295,
+    219, 228, 425,  82, 265, 144, 412, 449,  40, 435, 309, 362, 374,
+    223, 485, 392, 197, 366, 478, 433, 195, 479,  54, 238, 494, 240,
+    147,  73, 154, 438, 105, 129, 293,  11,  94, 180, 329, 455, 372,
+     62, 315, 439, 142, 454, 174,  16, 149, 495,  78, 242, 509, 133,
+    253, 246, 160, 367, 131, 138, 342, 155, 316, 263, 359, 152, 464,
+    489,   3, 510, 189, 290, 137, 210, 399,  18,  51, 106, 322, 237,
+    368, 283, 226, 335, 344, 305, 327,  93, 275, 461, 121, 353, 421,
+    377, 158, 436, 204,  34, 306,  26, 232,   4, 391, 493, 407,  57,
+    447, 471,  39, 395, 198, 156, 208, 334, 108,  52, 498, 110, 202,
+     37, 186, 401, 254,  19, 262,  47, 429, 370, 475, 192, 267, 470,
+    245, 492, 269, 118, 276, 427, 117, 268, 484, 345,  84, 287,  75,
+    196, 446, 247,  41, 164,  14, 496, 119,  77, 378, 134, 139, 179,
+    369, 191, 270, 260, 151, 347, 352, 360, 215, 187, 102, 462, 252,
+    146, 453, 111,  22,  74, 161, 313, 175, 241, 400,  10, 426, 323,
+    379,  86, 397, 358, 212, 507, 333, 404, 410, 135, 504, 291, 167,
+    440, 321,  60, 505, 320,  42, 341, 282, 417, 408, 213, 294, 431,
+     97, 302, 343, 476, 114, 394, 170, 150, 277, 239,  69, 123, 141,
+    325,  83,  95, 376, 178,  46,  32, 469,  63, 457, 487, 428,  68,
+     56,  20, 177, 363, 171, 181,  90, 386, 456, 468,  24, 375, 100,
+    207, 109, 256, 409, 304, 346,   5, 288, 443, 445, 224,  79, 214,
+    319, 452, 298,  21,   6, 255, 411, 166,  67, 136,  80, 351, 488,
+    289, 115, 382, 188, 194, 201, 371, 393, 501, 116, 460, 486, 424,
+    405,  31,  65,  13, 442,  50,  61, 465, 128, 168,  87, 441, 354,
+    328, 217, 261,  98, 122,  33, 511, 274, 264, 448, 169, 285, 432,
+    422, 205, 243,  92, 258,  91, 473, 324, 502, 173, 165,  58, 459,
+    310, 383,  70, 225,  30, 477, 230, 311, 506, 389, 140, 143,  64,
+    437, 190, 120,   0, 172, 272, 350, 292,   2, 444, 162, 234, 112,
+    508, 278, 348,  76, 450
 };
 
-#define FL_enc(k) {\
-r1 ^= r0 & EXTKEY[0][k];\
-r3 ^= r2 & EXTKEY[1][(k+2)&7];\
-r0 ^= r1 | EXTKEY[1][(k+6)&7];\
-r2 ^= r3 | EXTKEY[0][(k+4)&7];\
-}
-
-#define FL_dec(k) {\
-r0 ^= r1 | EXTKEY[0][(k+4)&7];\
-r2 ^= r3 | EXTKEY[1][(k+6)&7];\
-r1 ^= r0 & EXTKEY[1][(k+2)&7];\
-r3 ^= r2 & EXTKEY[0][k];\
-}
-
-#define FI_key(k) {\
-r0 = EXTKEY[0][k] >> 7;\
-r1 = EXTKEY[0][k] & 0x7f;\
-r0 = S9[r0] ^ r1;\
-r1 = S7[r1] ^ (r0 & 0x7f);\
-r1 ^= EXTKEY[0][(k+1)&7] >> 9;\
-r0 ^= EXTKEY[0][(k+1)&7] & 0x1ff;\
-r0 = S9[r0] ^ r1;\
-EXTKEY[3][k] = r1;\
-EXTKEY[2][k] = r0;\
-EXTKEY[1][k] = r1 << 9 ^ r0;\
-}
-
-#define FI_txt(a0, a1, k) {\
-a1 = a0 >> 7;\
-a0 &= 0x7f;\
-a1 = S9[a1] ^ a0;\
-a0 = S7[a0] ^ a1;\
-a1 ^= EXTKEY[2][k];\
-a0 ^= EXTKEY[3][k];\
-a0 &= 0x7f;\
-a1 = S9[a1] ^ a0;\
-a1 ^= a0 << 9;\
-}
-
-#define FO_txt(a0, a1, a2, a3, k) {\
-t0 = a0 ^ EXTKEY[0][k];\
-FI_txt(t0, t1, (k+5)&7);\
-t1 ^= a1;\
-t2 = a1 ^ EXTKEY[0][(k+2)&7];\
-FI_txt(t2, t0, (k+1)&7);\
-t0 ^= t1;\
-t1 ^= EXTKEY[0][(k+7)&7];\
-FI_txt(t1, t2, (k+3)&7);\
-t2 ^= t0;\
-t0 ^= EXTKEY[0][(k+4)&7];\
-a2 ^= t0;\
-a3 ^= t2;\
-}
-
-/***********************************************
-*
-* Encryption/Decryption Subroutine Body
-*
-* misty1(text, key, block, mode)
-*
-* text  : plain/ciphertext address I/O
-* key   : secret-key address
-* block : number of text blocks
-* mode  : 0: encryption 1: decryption
-*
-***********************************************/
-
-void misty1(unsigned char *text, unsigned char *key,
-    int block, int mode)
+unsigned int FI(unsigned int fi_in, unsigned int fi_key)
 {
-  register unsigned short t0, t1, t2;
-  register unsigned short r0, r1, r2, r3;
+    unsigned int d7, d9;
 
-  /*** Key Scheduling ***/
-  EXTKEY[0][0] = (unsigned short)key[0]<<8 ^ (unsigned short)key[1];
-  EXTKEY[0][1] = (unsigned short)key[2]<<8 ^ (unsigned short)key[3];
-  EXTKEY[0][2] = (unsigned short)key[4]<<8 ^ (unsigned short)key[5];
-  EXTKEY[0][3] = (unsigned short)key[6]<<8 ^ (unsigned short)key[7];
-  EXTKEY[0][4] = (unsigned short)key[8]<<8 ^ (unsigned short)key[9];
-  EXTKEY[0][5] = (unsigned short)key[10]<<8 ^ (unsigned short)key[11];
-  EXTKEY[0][6] = (unsigned short)key[12]<<8 ^ (unsigned short)key[13];
-  EXTKEY[0][7] = (unsigned short)key[14]<<8 ^ (unsigned short)key[15];
-  FI_key(0);
-  FI_key(1);
-  FI_key(2);
-  FI_key(3);
-  FI_key(4);
-  FI_key(5);
-  FI_key(6);
-  FI_key(7);
+    d9 = s9[((fi_in >> 7) & 0x1ff)] ^ ((fi_in & 0x7f)); 
+    d7 = (fi_in & 0x7f);
+    d7 = ((s7[d7] ^ d9) & 0x7f) ^ ((fi_key >> 9) & 0x7f);
+    d9 ^= (fi_key & 0x1ff);
+    d9 = s9[d9] ^ d7;
+    return ((d7 << 9) | d9);
+}
 
-  /*** Data Randomizing ***/
-  if(!(mode & 1)) {
-    /*** Encryption ***/
-    while(block-- > 0) {
-      r0 = (unsigned short)text[0]<<8 ^ (unsigned short)text[1];
-      r1 = (unsigned short)text[2]<<8 ^ (unsigned short)text[3];
-      r2 = (unsigned short)text[4]<<8 ^ (unsigned short)text[5];
-      r3 = (unsigned short)text[6]<<8 ^ (unsigned short)text[7];
-      FL_enc(0);
-      FO_txt(r0, r1, r2, r3, 0);
-      FO_txt(r2, r3, r0, r1, 1);
-      FL_enc(1);
-      FO_txt(r0, r1, r2, r3, 2);
-      FO_txt(r2, r3, r0, r1, 3);
-      FL_enc(2);
+void keyinit(unsigned char *inputkey, unsigned int *EK)
+{
+    int i;
 
-      FO_txt(r0, r1, r2, r3, 4);
-      FO_txt(r2, r3, r0, r1, 5);
-      FL_enc(3);
-      FO_txt(r0, r1, r2, r3, 6);
-      FO_txt(r2, r3, r0, r1, 7);
-      FL_enc(4);
+    for (i = 0; i < 8; i++)
+        EK[i] = (inputkey[i*2] * 256) + inputkey[i*2+1];
 
-      text[0] = r2 >> 8;
-      text[1] = r2 & 0xff;
-      text[2] = r3 >> 8;
-      text[3] = r3 & 0xff;
-      text[4] = r0 >> 8;
-      text[5] = r0 & 0xff;
-      text[6] = r1 >> 8;
-      text[7] = r1 & 0xff;
-      text += 8;
+    for (i = 0; i < 8; i++) {
+        EK[i+ 8] = FI(EK[i], EK[(i+1) % 8]);
+        EK[i+16] = EK[i+8] & 0x1ff;
+        EK[i+24] = EK[i+8] >> 9;
     }
-  } else {
-    /*** Decryption ***/
-    while(block-- > 0) {
-      r0 = (unsigned short)text[0]<<8 ^ (unsigned short)text[1];
-      r1 = (unsigned short)text[2]<<8 ^ (unsigned short)text[3];
-      r2 = (unsigned short)text[4]<<8 ^ (unsigned short)text[5];
-      r3 = (unsigned short)text[6]<<8 ^ (unsigned short)text[7];
+}
 
-      FL_dec(4);
-      FO_txt(r0, r1, r2, r3, 7);
-      FO_txt(r2, r3, r0, r1, 6);
-      FL_dec(3);
-      FO_txt(r0, r1, r2, r3, 5);
-      FO_txt(r2, r3, r0, r1, 4);
-      FL_dec(2);
-      FO_txt(r0, r1, r2, r3, 3);
-      FO_txt(r2, r3, r0, r1, 2);
-      FL_dec(1);
-      FO_txt(r0, r1, r2, r3, 1);
-      FO_txt(r2, r3, r0, r1, 0);
-      FL_dec(0);
+unsigned int FO(unsigned int fo_in, int k)
+{
+    unsigned int t0, t1;
 
-      text[0] = r2 >> 8;
-      text[1] = r2 & 0xff;
-      text[2] = r3 >> 8;
-      text[3] = r3 & 0xff;
-      text[4] = r0 >> 8;
-      text[5] = r0 & 0xff;
-      text[6] = r1 >> 8;
-      text[7] = r1 & 0xff;
-      text += 8;
+    t0 = fo_in >> 16;
+    t1 = fo_in & 0xffff;
+    t0 = t0 ^ EK[k];
+    t0 = FI(t0, EK[((k+5)%8)+8]);
+    t0 = t0 ^ t1;
+    t1 = t1 ^ EK[(k+2)%8];
+    t1 = FI(t1, EK[((k+1)%8)+8]);
+    t1 = t1 ^ t0;
+    t0 = t0 ^ EK[(k+7)%8];
+    t0 = FI(t0, EK[((k+3)%8)+8]);
+    t0 = t0 ^ t1;
+    t1 = t1 ^ EK[(k+4)%8];
+    return((t1 << 16) | t0);
+}
+
+unsigned int FL(unsigned int fl_in, int k)
+{
+    unsigned int d0, d1;
+
+    d0 = fl_in >> 16;
+    d1 = fl_in & 0xffff;
+    if (k % 2 == 0) {
+        d1 = d1 ^ (d0 & EK[k/2]);
+        d0 = d0 ^ (d1 | EK[(((k/2)+6)%8)+8]);
+    } else {
+        d1 = d1 ^ (d0 & EK[((((k-1)/2)+2)%8)+8]);
+        d0 = d0 ^ (d1 | EK[(((k-1)/2)+4)%8]);
     }
-  }
+
+    return((d0 << 16) | d1);
+}
+
+unsigned int FLINV(unsigned int fl_in, int k)
+{
+    unsigned int d0, d1;
+
+    d0 = fl_in >> 16;
+    d1 = fl_in & 0xffff;
+    if (k % 2 == 0) {
+        d0 = d0 ^ (d1 | EK[(((k/2)+6)%8)+8]);
+        d1 = d1 ^ (d0 & EK[k/2]);
+    } else {
+        d0 = d0 ^ (d1 | EK[(((k-1)/2)+4)%8]);
+        d1 = d1 ^ (d0 & EK[((((k-1)/2)+2)%8)+8]);
+    }
+
+    return((d0 << 16) | d1);
+}
+
+void misty1_encrypt(unsigned int *expkey, unsigned char *ptext,
+    unsigned char *ctext)
+{
+    unsigned int D0, D1;
+    int i, j = 0;
+
+    for (i = 0; i < 32; i++) EK[i] = expkey[i];
+
+    D0 = D1 = 0;
+
+    for (i = 0; i < 4; i++) {
+        D0 = D0 << 8;
+        D0 |= ptext[j++];
+    }
+
+    for (i = 0; i < 4; i++) {
+        D1 = D1 << 8;
+        D1 |= ptext[j++];
+    }
+
+    /* round 0 */
+    D0 = FL(D0, 0);
+    D1 = FL(D1, 1);
+    D1 = D1 ^ FO(D0, 0);
+
+    /* round 1 */
+    D0 = D0 ^ FO(D1, 1);
+
+    /* round 2 */
+    D0 = FL(D0, 2);
+    D1 = FL(D1, 3);
+    D1 = D1 ^ FO(D0, 2);
+
+    /* round 3 */
+    D0 = D0 ^ FO(D1, 3);
+
+    /* round 4 */
+    D0 = FL(D0, 4);
+    D1 = FL(D1, 5);
+    D1 = D1 ^ FO(D0, 4);
+
+    /* round 5 */
+    D0 = D0 ^ FO(D1, 5);
+
+    /* round 6 */
+    D0 = FL(D0, 6);
+    D1 = FL(D1, 7);
+    D1 = D1 ^ FO(D0, 6);
+
+    /* round 7 */
+    D0 = D0 ^ FO(D1, 7);
+
+    /* final */
+    D0 = FL(D0, 8);
+    D1 = FL(D1, 9);
+
+    ctext[0] = (D1 >> 24) & 0xff;
+    ctext[1] = (D1 >> 16) & 0xff;
+    ctext[2] = (D1 >> 8) & 0xff;
+    ctext[3] = D1 & 0xff;
+    ctext[4] = (D0 >> 24) & 0xff;
+    ctext[5] = (D0 >> 16) & 0xff;
+    ctext[6] = (D0 >> 8) & 0xff;
+    ctext[7] = D0 & 0xff;
+}
+
+void misty1_decrypt(unsigned int expkey[32], unsigned char *ctext,
+    unsigned char *ptext)
+{
+    unsigned int D0, D1;
+    int i, j = 0;
+
+    for (i = 0; i < 32; i++) EK[i] = expkey[i];
+
+    D0 = D1 = 0;
+
+    for (i = 0; i < 4; i++) {
+        D1 = D1 << 8;
+        D1 |= ctext[j++];
+    }
+
+    for (i = 0; i < 4; i++) {
+        D0 = D0 << 8;
+        D0 |= ctext[j++];
+    }
+
+    D0 = FLINV(D0, 8);
+    D1 = FLINV(D1, 9);
+    D0 = D0 ^ FO(D1, 7);
+    D1 = D1 ^ FO(D0, 6);
+    D0 = FLINV(D0, 6);
+    D1 = FLINV(D1, 7);
+    D0 = D0 ^ FO(D1, 5);
+    D1 = D1 ^ FO(D0, 4);
+    D0 = FLINV(D0, 4);
+    D1 = FLINV(D1, 5);
+    D0 = D0 ^ FO(D1, 3);
+    D1 = D1 ^ FO(D0, 2);
+    D0 = FLINV(D0, 2);
+    D1 = FLINV(D1, 3);
+    D0 = D0 ^ FO(D1, 1);
+    D1 = D1 ^ FO(D0, 0);
+    D0 = FLINV(D0, 0);
+    D1 = FLINV(D1, 1);
+
+    ptext[0] = (D0 >> 24) & 0xff;
+    ptext[1] = (D0 >> 16) & 0xff;
+    ptext[2] = (D0 >> 8) & 0xff;
+    ptext[3] = D0 & 0xff;
+    ptext[4] = (D1 >> 24) & 0xff;
+    ptext[5] = (D1 >> 16) & 0xff;
+    ptext[6] = (D1 >> 8) & 0xff;
+    ptext[7] = D1 & 0xff;
 }
 
 int main(void)
 {
-  static unsigned char text[16] = {
-    0x01,0x23,0x45,0x67,0x89,0xab,0xcd,0xef,
-    0xfe,0xdc,0xba,0x98,0x76,0x54,0x32,0x10
-  };
+    /********************************
+    old plaintext1 : 0123456789abcdef
+    ciphertext1    : 8b1da5f56ab3d07c
+    new plaintext1 : 0123456789abcdef
 
-  static unsigned char key[16] = {
-    0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,
-    0x88,0x99,0xaa,0xbb,0xcc,0xdd,0xee,0xff
-  };
+    old plaintext2 : fedcba9876543210
+    ciphertext2    : 04b68240b13be95d
+    new plaintext2 : fedcba9876543210
+    *********************************/
 
-  int i;
+    int i;
 
-  printf("Secret Key          : ");
-  for(i = 0; i < 16; i++) printf("%02x ", key[i]);
-  printf("\n");
+    static unsigned char plaintext1[8] = {
+        0x01,0x23,0x45,0x67,0x89,0xab,0xcd,0xef
+    };
 
-  printf("Orig Plaintext      : ");
-  for(i = 0; i < 16; i++) printf("%02x ", text[i]);
-  printf("\n");
+    static unsigned char plaintext2[8] = {
+        0xfe,0xdc,0xba,0x98,0x76,0x54,0x32,0x10
+    };
 
-  misty1(text, key, 2, 0);
-  printf("Extended Key        : ");
-  for(i = 0; i < 8; i++)
-    printf("%02x %02x ", (unsigned char)(EXTKEY[1][i]>>8),
-        (unsigned char)(EXTKEY[1][i]&0xff));
-  printf("\n");
+    static unsigned char key[16] = {
+        0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,
+        0x88,0x99,0xaa,0xbb,0xcc,0xdd,0xee,0xff
+    };
 
-  printf("Ciphertext          : ");
-  for(i = 0; i < 16; i++) printf("%02x ", text[i]);
-  printf("\n");
+    static unsigned char ciphertext1[8];
+    static unsigned char ciphertext2[8];
+    static unsigned char decryptedplaintext1[8];
+    static unsigned char decryptedplaintext2[8];
+    unsigned int expandedkey[32];
 
-  misty1(text, key, 2, 1);
-  printf("Decrypted Plaintext : ");
-  for(i = 0; i < 16; i++) printf("%02x ", text[i]);
-  printf("\n");
+    keyinit(key, expandedkey);
+    printf("old plaintext1 : ");
+    for (i = 0; i < 8; i++) printf("%02x", plaintext1[i]);
+    printf("\n");
 
-  return 0;
+    misty1_encrypt(expandedkey, plaintext1, ciphertext1);
+    printf("ciphertext1    : ");
+    for (i = 0; i < 8; i++) printf("%02x", ciphertext1[i]);
+    printf("\n");
+
+    misty1_decrypt(expandedkey, ciphertext1, decryptedplaintext1);
+    printf("new plaintext1 : ");
+    for (i = 0; i < 8; i++) printf("%02x", decryptedplaintext1[i]);
+    printf("\n\n");
+
+    printf("old plaintext2 : ");
+    for (i = 0; i < 8; i++) printf("%02x", plaintext2[i]);
+    printf("\n");
+
+    misty1_encrypt(expandedkey, plaintext2, ciphertext2);
+    printf("ciphertext2    : ");
+    for (i = 0; i < 8; i++) printf("%02x", ciphertext2[i]);
+    printf("\n");
+
+    misty1_decrypt(expandedkey, ciphertext2, decryptedplaintext2);
+    printf("new plaintext2 : ");
+    for (i = 0; i < 8; i++) printf("%02x", decryptedplaintext2[i]);
+    printf("\n");
+
+    return 0;
 }
 
